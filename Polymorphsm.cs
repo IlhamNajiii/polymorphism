2fb1ca5f9306cb1e96c5f2106a9556683fb8303d
IlhamNajiii <127828076+IlhamNajiii@users.noreply.github.com>
2023-06-27 14:48:03 +0700

    Add files via upload

---
+++ Polymorphsm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphsm
+{
+    abstract class printerWindows
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer Windows display dimension: ");
+        }
+        public abstract void Print();
+    }
+    class Epson : printerWindows
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Epson Printer printing...");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("Epson Windows display dimension: 10 * 11");
+        }
+    }
+
+    class Canon : printerWindows
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Canon Printer printing...");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("Epson Windows display dimension: 9.5 * 12");
+        }
+    }
+    class LaserJet : printerWindows
+    {
+        public override void Print()
+        {
+            Console.WriteLine("LaserJet Printer printing...");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("Epson Windows display dimension: 12 * 12");
+        }
+    }
+}
